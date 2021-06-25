4a337c922c4ca20baac3fac96a5ef49e3ae8a10e
leonel47 <waboleonel@gmail.com>
2021-06-25 11:51:19 -0400

    Code refactoring, container alignment

--- src/App.js
+++ src/App.js
@@ -7,26 +7,27 @@ import AppMap from "./components/location/Map";
 function App() {
     return (
         <>
-            <div className="container-sm mt-4">
-                <h1 className="text-center">Contacts</h1>
-                <p className="text-muted text-center m-4" style={{fontSize: '14px'}}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and
-                    scrambled it to make a type specimen book.
-                </p>
-                <div className="row mt-4">
-                    <div className="col-md-5">
-                        <div className="p-0 shadow-sm">
-                            <LocationDetails/>
-                            <AppMap/>
+            <div className="container">
+                <div className="container mt-4">
+                    <h1 className="text-center">Contacts</h1>
+                    <p className="text-muted text-center m-4" style={{fontSize: '14px'}}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        type and scrambled it to make a type specimen book.
+                    </p>
+                    <div className="row mt-4">
+                        <div className="col-md-5">
+                            <div className="p-0 shadow-sm">
+                                <LocationDetails/>
+                                <AppMap/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-7 shadow-sm">
-                        <div className="p-1">
-                            <RegistrationForm/>
-                        </div>
+                        <div className="col-md-7 shadow-sm">
+                            <div className="p-1">
+                                <RegistrationForm/>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
